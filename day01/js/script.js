1a26de80f38442edd8b6b286f5a5848c766897d0
Matthew Hovsepian <mlthovsep@hotmail.com>
2017-05-15 20:57:25 -0700

    day01

---
+++ day01/js/script.js
@@ -0,0 +1,9 @@
+// ES6 - ECMAScript6
+
+console.log('Sending a message to console.');
+
+console.info('Sending information message.');
+
+console.warn('Here\'s a warning.');
+
+console.error('Danger Will Robinson!');
